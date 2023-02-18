3504e41b23a14169666191def7cf46643f13e01b
DeokgyunSHIN(신덕균) <104719555+DeokgyunSHIN@users.noreply.github.com>
2023-02-18 23:27:34 +0900

    Add files via upload

---
+++ POST/post/src/main/java/com/example/post/dto/PostRequestDto.java
@@ -0,0 +1,75 @@
+package com.example.post.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PostRequestDto {
+    private String account;
+    private String email;
+    private String address;
+    private String password;
+    @JsonProperty("OTP")
+    private String OTP;
+
+    @JsonProperty("phone_number")  // 단점은 일일히 달아줘야하는 불편함이 존재한다.
+    private String phoneNumber; //phone_number
+
+    public String getOTP() {
+        return OTP;
+    }
+
+    public void setOTP(String OTP) {
+        this.OTP = OTP;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PostRequestDto{" +
+                "account='" + account + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                ", OTP='" + OTP + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
