58f9a6062393184c6ad23099a775e8e0b93e747c
DeokgyunSHIN(신덕균) <104719555+DeokgyunSHIN@users.noreply.github.com>
2023-02-18 23:29:00 +0900

    Add files via upload

---
+++ DELETE/DELECT/src/test/java/com/example/delect/DelectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.delect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
